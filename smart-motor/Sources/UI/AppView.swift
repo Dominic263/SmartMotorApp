1560819eebd5c1b1f01c7cbcd83323d684fa3cfc
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2022-12-08 06:31:11 -0500

    test

--- smart-motor/Sources/UI/AppView.swift
+++ smart-motor/Sources/UI/AppView.swift
@@ -10,7 +10,9 @@ import ComposableArchitecture
 
 struct AppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Smart Motor")
+            .font(.title)
+            
     }
 }
 
